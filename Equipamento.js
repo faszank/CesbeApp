88f186a8105649745f7a2ccee4e35d822c591725
faszank <nelio.fjr@gmail.com>
2024-05-14 09:18:07 -0300

    Create Equipamento.js

---
+++ Equipamento.js
@@ -0,0 +1,9 @@
+{
+   "id_equipamento": 9,
+   "descricao": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   "tipol": "Escavadeira",
+   "capacidade": 5,
+   "data": {
+      "unidade_medida": "M"
+   }
+}
