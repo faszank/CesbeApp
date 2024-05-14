3f929bd89ee0ded94dbe42119092ab32a07922b1
faszank <nelio.fjr@gmail.com>
2024-05-14 09:21:47 -0300

    Update Equipamento.js

--- Equipamento.js
+++ Equipamento.js
@@ -1,9 +1,7 @@
 {
-   "id_equipamento": 9,
+   "id_equipamento": 5,
    "descricao": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-   "tipol": "Escavadeira",
-   "capacidade": 5,
-   "data": {
-      "unidade_medida": "M"
-   }
+   "tipo": "Caminhão",
+   "unidade_medida": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+   "capacidade": 4
 }
